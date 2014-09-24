e1784de0f114c0469f04a492979ae568970b36ba
Hans Werner <hanzo87@gmail.com>
2014-09-24 00:53:14 -0700

    problem 5 - hamming distance

---
+++ Exercism/csharp/hamming/Hamming.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercism.csharp.hamming
+{
+	internal class Hamming
+	{
+		public static int Compute(string strand1, string strand2)
+		{
+			var diffCount = 0;
+
+			var minStrandLength = Math.Min(strand1.Length, strand2.Length);
+
+			for (var x = 0; x < minStrandLength; x++)
+			{
+				if (strand1[x] != strand2[x])
+					diffCount++;
+			}
+
+			return diffCount;
+		}
+	}
+}
