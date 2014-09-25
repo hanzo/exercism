1b8f1f03a1731b66b853a0c10d05854d7865d357
Hans Werner <hanzo87@gmail.com>
2014-09-25 12:53:48 -0700

    grade school, first iteration

---
+++ Exercism/csharp/grade-school/School.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Exercism.csharp.grade_school
+{
+	internal class School
+	{
+		private readonly Dictionary<int, List<string>> _roster;
+
+		public Dictionary<int, List<string>> Roster
+		{
+			get { return new Dictionary<int, List<string>>(_roster); }
+		}
+
+		public School()
+		{
+			_roster = new Dictionary<int, List<string>>();
+		}
+
+		public void Add(string name, int grade)
+		{
+			if (_roster.ContainsKey(grade))
+				_roster[grade].Add(name);
+			else
+				_roster[grade] = new List<string> {name};
+
+			_roster[grade].Sort();
+		}
+
+		public List<string> Grade(int grade)
+		{
+			return _roster.ContainsKey(grade) ? _roster[grade] : new List<string>();
+		}
+	}
+}
