5db8376c67ccc8f074a6a1a1d176ac34bf8fe7df
Hans Werner <hanzo87@gmail.com>
2014-09-26 17:07:05 -0700

    dna v1

---
+++ Exercism/csharp/nucleotide-count/DNA.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercism.csharp.nucleotide_count
+{
+	internal class DNA
+	{
+		private readonly Dictionary<char, int> _nucleotideCounts;
+
+		public DNA(string sequence)
+		{
+			_nucleotideCounts = new Dictionary<char, int>
+			{
+				{'A', 0},
+				{'T', 0},
+				{'C', 0},
+				{'G', 0},
+			};
+
+			CountNucleotides(sequence);
+		}
+
+		public int Count(char nucleotide)
+		{
+			if (!_nucleotideCounts.ContainsKey(nucleotide))
+				throw new InvalidNucleotideException();
+
+			return _nucleotideCounts[nucleotide];
+		}
+
+		public Dictionary<char, int> NucleotideCounts
+		{
+			get { return new Dictionary<char, int>(_nucleotideCounts); }
+		}
+
+		private void CountNucleotides(string sequence)
+		{
+			foreach (char currentChar in sequence.Where(c => _nucleotideCounts.ContainsKey(c)))
+				_nucleotideCounts[currentChar] ++;
+		}
+	}
+
+	internal class InvalidNucleotideException : ApplicationException
+	{
+	}
+}
