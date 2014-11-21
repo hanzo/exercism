e920fe050fa31f44ab3154e00c4770cfcbde51c3
hanzo <hanzo87@gmail.com>
2014-11-20 21:49:25 -0800

    sum of multiples v2

---
+++ Exercism/csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercism.csharp.sum_of_multiples
+{
+	class SumOfMultiples
+	{
+		private readonly List<int> _baseNumbers;
+
+		public SumOfMultiples()
+		{
+			_baseNumbers = new List<int> { 3, 5 };
+		}
+
+		public SumOfMultiples(List<int> baseNumbers)
+		{
+			_baseNumbers = baseNumbers;
+		}
+
+		public int To(int toNumber)
+		{
+			return Enumerable.Range(0, toNumber).Where(x => _baseNumbers.Any(b => x % b == 0)).Sum();
+		}
+	}
+}
