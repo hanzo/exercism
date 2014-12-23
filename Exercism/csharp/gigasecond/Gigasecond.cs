43ba59165fd7cf54e5ccc45e41ebecbd79876875
hanzo <hanzo87@gmail.com>
2014-12-22 18:20:27 -0600

    gigasecond v1

---
+++ Exercism/csharp/gigasecond/Gigasecond.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercism.csharp.gigasecond
+{
+	internal class Gigasecond
+	{
+		private DateTime _birthday;
+		private readonly double SecondsPerGigasecond = Math.Pow(10, 9);
+
+		public Gigasecond(DateTime birthday)
+		{
+			_birthday = birthday;
+		}
+
+		public DateTime Date()
+		{
+			return _birthday.AddSeconds(SecondsPerGigasecond).Date;
+		}
+	}
+}
