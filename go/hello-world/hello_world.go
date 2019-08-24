ee6d813513a74192b8e039ed041f8c3de0be2548
Hans Werner <hwerner@lyft.com>
2019-08-23 21:24:18 -0700

    go / hello world

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,7 @@
+// Greeting package contains functions for printing greetings.
+package greeting
+
+// HelloWorld returns a string saying 'Hello, World!'
+func HelloWorld() string {
+	return "Hello, World!"
+}
