82dc31f0c1f518ec34a0f11efd19f7f5d762df03
Hans Werner <hwerner@lyft.com>
2019-09-14 11:59:41 -0700

    go / scrabble v2

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,47 +1,38 @@
 // Package scrabble contains functions for scoring Scrabble words
 package scrabble
 
-import "strings"
+import "unicode"
 
-var LetterScores = map[rune]int{
-	'A': 1,
-	'E': 1,
-	'I': 1,
-	'O': 1,
-	'U': 1,
-	'L': 1,
-	'N': 1,
-	'R': 1,
-	'S': 1,
-	'T': 1,
-	'D': 2,
-	'G': 2,
-	'B': 3,
-	'C': 3,
-	'M': 3,
-	'P': 3,
-	'F': 4,
-	'H': 4,
-	'V': 4,
-	'W': 4,
-	'Y': 4,
-	'K': 5,
-	'J': 8,
-	'X': 8,
-	'Q': 10,
-	'Z': 10,
+// ScoreLetter returns the scrabble score for the given letter.
+// Returns zero for non-English letters.
+func ScoreLetter(letter rune) int {
+	switch unicode.ToUpper(letter) {
+	case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+		return 1
+	case 'D', 'G':
+		return 2
+	case 'B', 'C', 'M', 'P':
+		return 3
+	case 'F', 'H', 'V', 'W', 'Y':
+		return 4
+	case 'K':
+		return 5
+	case 'J', 'X':
+		return 8
+	case 'Q', 'Z':
+		return 10
+	default:
+		return 0
+	}
 }
 
 // Score returns the scrabble score for the given word.
 func Score(word string) int {
 	var score int
-	word = strings.ToUpper(word)
-
 	// note that `range word` is the equivalent of `range []rune(word)`
 	// https://staticcheck.io/docs/checks#S1029
 	for _, rune := range word {
-		// this lookup will return 0 for keys that aren't found
-		score += LetterScores[rune]
+		score += ScoreLetter(rune)
 	}
 	return score
 }
