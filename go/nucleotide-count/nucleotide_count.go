1c4e1952569a86bcf790fad792b6a6f1feb29566
Hans Werner <hwerner@lyft.com>
2020-08-07 14:31:22 -0700

    go / nucleotide count v2

--- go/nucleotide-count/nucleotide_count.go
+++ go/nucleotide-count/nucleotide_count.go
@@ -12,17 +12,16 @@ type Histogram map[rune]int
 // DNA is a list of nucleotides
 type DNA string
 
-// Nucleotides is a static list of valid nucleotide types
-var Nucleotides = [4]rune{'A', 'C', 'G', 'T'}
-
 // Counts generates a histogram of valid nucleotides in the given DNA.
 // Returns an error if d contains an invalid nucleotide.
 func (d DNA) Counts() (Histogram, error) {
 
 	// initialize the histogram with a count of zero for each nucleotide type
-	var h Histogram = make(Histogram)
-	for _, nuc := range Nucleotides {
-		h[nuc] = 0
+	var h Histogram = Histogram{
+		'A': 0,
+		'C': 0,
+		'G': 0,
+		'T': 0,
 	}
 
 	dna := strings.ToUpper(string(d))
