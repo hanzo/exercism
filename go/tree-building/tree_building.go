686cb1ade75127bd635e98c907dc25b985d2ba5d
Hans Werner <hwerner@lyft.com>
2020-08-11 10:41:35 -0700

    go / tree building v4

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -17,26 +17,22 @@ type Node struct {
 	Children []*Node
 }
 
-// the constraints of the question imply that the root node ID must be zero
-const rootNodeID = 0
-
 // Build returns the root node of a tree representation of the given list of records.
 func Build(records []Record) (*Node, error) {
-	nodeLookup := make(map[int]*Node)
+	nodes := make(map[int]*Node)
 	// sort records by ID in ascending order
 	sort.Slice(records, func(i, j int) bool {
 		return records[i].ID < records[j].ID
 	})
 	for i, r := range records {
-		parent, foundParent := nodeLookup[r.Parent]
-		if r.ID != i || (r.ID == rootNodeID && r.ID != r.Parent) || (r.ID > rootNodeID && !foundParent) {
+		if r.ID != i || r.Parent > r.ID || r.ID > 0 && r.Parent == r.ID {
 			return nil, fmt.Errorf("invalid record with ID %d and parent ID %d", r.ID, r.Parent)
 		}
-		node := &Node{ID: r.ID}
-		nodeLookup[r.ID] = node
-		if r.ID > rootNodeID {
-			parent.Children = append(parent.Children, node)
+		nodes[r.ID] = &Node{ID: r.ID}
+		if r.ID > 0 {
+			p := nodes[r.Parent]
+			p.Children = append(p.Children, nodes[r.ID])
 		}
 	}
-	return nodeLookup[0], nil
+	return nodes[0], nil
 }
